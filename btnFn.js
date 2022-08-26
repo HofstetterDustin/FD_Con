06de8e0e6ed16c6b1dffa0aac5fd2ee6610bd381
Dustin Hofstetter <hofstetter.dustinart@gmail.com>
2022-08-26 19:44:28 -0400

    Adding C# lang with dotnet new mvc basics.

--- btnFn.js
+++ btnFn.js
@@ -1,5 +1,16 @@
-function dpContent() {
-    let CRT1 = document.getElementById("C2A2")
-    return (CRT1);
 
+let crt1PowerOn = C2S1function
+
+/*crt1PowerOn*/
+
+function C2S1ON() {
+    var C2S1 = "";
+    if (crt1PowerOn == true) {
+        C2S1 = "ON";
+    } else {
+        C2S1 = "ST BY";
+    }
+    document.getElementById("C2A2").innerHTML = C2S1;
+    console.log(C2S1);
+    return C2S1;
 }
